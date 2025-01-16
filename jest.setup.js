4ec72888465ebf1d1d7052b047d2e411bf55ffdf
radim.brnka <rbrnka@gmail.com>
2025-01-16 12:25:47 +0100

    jest setup adjustments (clipboard mock), prep for touch tests

--- jest.setup.js
+++ jest.setup.js
@@ -6,3 +6,23 @@ document.body.innerHTML = `
   <button id="randomize">Randomize Colors</button>
   <button id="preset1">Preset 1</button>
 `;
+
+// jest.setup.js
+Object.defineProperty(navigator, 'clipboard', {
+    value: {
+        writeText: jest.fn().mockResolvedValue(),
+    },
+    writable: true,
+});
+
+if (typeof Touch === 'undefined') {
+    global.Touch = class Touch {
+        constructor({ identifier, target, clientX, clientY }) {
+            this.identifier = identifier;
+            this.target = target;
+            this.clientX = clientX;
+            this.clientY = clientY;
+            // You can add other properties as needed.
+        }
+    };
+}
