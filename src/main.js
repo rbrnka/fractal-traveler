95cd9bfe17830ec1f99956017376944567fd7db1
Radim Brnka <71871287+rbrnka@users.noreply.github.com>
2025-01-30 13:06:46 +0100

    Refactor: Simplify

--- src/main.js
+++ src/main.js
@@ -4,39 +4,20 @@
  */
 import './css/style.css';
 import {JuliaRenderer} from "./juliaRenderer";
-import {enableJuliaMode, initUI, MODE_JULIA, MODE_MANDELBROT} from "./ui";
 import {MandelbrotRenderer} from "./mandelbrotRenderer";
+import {enableJuliaMode, initUI, MODE_JULIA, MODE_MANDELBROT} from "./ui";
 import {clearURLParams, loadFractalParamsFromURL} from "./utils";
 
 document.addEventListener('DOMContentLoaded', () => {
-        console.log('Basic test: DOMContentLoaded triggered');
+        console.log('DOMContentLoaded triggered');
 
         // Create the fractal application instance.
         const canvas = document.getElementById('fractalCanvas');
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
-        let fractalApp;
-
         const params = loadFractalParamsFromURL();
-        let initializedFromParams = false;
-
-        // http://localhost:8080/#julia?view=eyJtb2RlIjoiMS4wIiwicHgiOiItMC4yOTA0MjIiLCJweSI6Ii0wLjE3NzQ5MyIsInpvb20iOiIwLjAwMDAyNiIsInIiOiItMC42NDkyMDQiLCJjeCI6IjAuMzU1MDAwIiwiY3kiOiIwLjUyMDAwMCJ9
-        // http://localhost:8080/#?view=eyJtb2RlIjpudWxsLCJweCI6Ii0wLjcyMTYwNCIsInB5IjoiMC4yNTkxMjYiLCJ6b29tIjoiMC4wMDAwMzgiLCJyIjoiNC42NzAwMDAiLCJjeCI6bnVsbCwiY3kiOm51bGx9
-
         console.log("URL: " + JSON.stringify(params));
-
-        if (params.mode === MODE_JULIA) {
-            enableJuliaMode();
-            fractalApp = new JuliaRenderer(canvas);
-            console.log("Constructing Julia");
-        } else {
-            console.log("Constructing Mandelbrot");
-            fractalApp = new MandelbrotRenderer(canvas);
-        }
-
-        fractalApp.init();
-        console.log('Fractal initialized');
 
         // Do we have all the params needed for initial travel?
         const preset = {
@@ -45,32 +26,50 @@ document.addEventListener('DOMContentLoaded', () => {
             c: [params.cx, params.cy],
             rotation: params.r
         };
-
         console.log("Preset: " + JSON.stringify(preset));
 
-        if (params.mode === MODE_MANDELBROT && params.px && params.py && params.zoom && params.r != null) {
-            fractalApp.animatePanZoomRotate(preset.pan, preset.zoom, preset.rotation, 500);
-            initializedFromParams = true;
-            console.log("Traveling to URL params! M");
-        } else if (params.mode === MODE_JULIA && params.px && params.py && params.zoom && params.r && params.cx && params.cy) {
-            fractalApp.animateTravelToPreset(preset, 500);
-            initializedFromParams = true;
-            console.log("Traveling to URL params! J");
-        } else {
+        const onDefault = () => {
+            fractalApp.reset();
             clearURLParams();
-            console.log("Initialized default fractal (no or invalid URL params) ");
+        }
+
+        let fractalApp;
+
+        switch (params.mode) {
+            case MODE_JULIA:
+                enableJuliaMode();
+                fractalApp = new JuliaRenderer(canvas);
+                fractalApp.init();
+                if (params.px && params.py && params.zoom && params.r && params.cx && params.cy) {
+                    fractalApp.animateTravelToPreset(preset, 500);
+                    console.log("Constructing Julia from URL params");
+                } else {
+                    console.log("Constructing default Julia");
+                    onDefault();
+                }
+                break;
+
+            case MODE_MANDELBROT:
+                fractalApp = new MandelbrotRenderer(canvas);
+                fractalApp.init();
+                if (params.px && params.py && params.zoom && params.r) {
+                    fractalApp.animatePanZoomRotate(preset.pan, preset.zoom, preset.rotation, 500);
+                    console.log("Constructing Mandelbrot from URL params");
+                } else {
+                    console.log("Constructing default Mandelbrot");
+                    onDefault();
+                }
+                break;
+
+            default:
+                console.error('Unknown fractal mode: ' + params.mode);
+                break;
         }
 
         initUI(fractalApp);
-
-        if (!initializedFromParams) {
-            fractalApp.reset();
-        }
-
         console.log('Init complete.');
     },
     {
         once: true
     }
-)
-;
+);
