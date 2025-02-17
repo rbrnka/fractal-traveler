bf93b988c5a0811b7949ae44b7ce69e018b69e57
Radim Brnka <71871287+rbrnka@users.noreply.github.com>
2025-02-17 13:17:23 +0100

    1.8 - Fixed double right click zoom-out and rotation reset

--- src/ui/mouseEventHandlers.js
+++ src/ui/mouseEventHandlers.js
@@ -6,7 +6,7 @@
 
 import {calculatePanDelta, expandComplexToString, normalizeRotation, updateURLParams} from '../global/utils.js';
 import {isJuliaMode, resetAppState, toggleDebugLines, updateInfo} from './ui.js';
-import {DEFAULT_CONSOLE_GROUP_COLOR, FRACTAL_TYPE} from "../global/constants";
+import {DEBUG_MODE, DEFAULT_CONSOLE_GROUP_COLOR, EASE_TYPE, FRACTAL_TYPE} from "../global/constants";
 
 /** How long should we wait before distinguish between double click and two single clicks. */
 const DOUBLE_CLICK_THRESHOLD = 300;
@@ -31,6 +31,7 @@ let mouseDownX = 0, mouseDownY = 0;
 let lastX = 0, lastY = 0;
 let clickTimeout = null;
 let isDragging = false;
+let wasRotated = false;
 
 // Rotation
 let isRightDragging = false;
@@ -129,16 +130,15 @@ function handleMouseDown(event) {
         lastX = event.clientX;
         lastY = event.clientY;
     } else if (event.button === 2) { // Right-click
-        isRightDragging = true;
         startX = event.clientX;
     }
 }
 
 function handleMouseMove(event) {
-    if (event.buttons === 1) {
-        const dx = event.clientX - mouseDownX;
-        const dy = event.clientY - mouseDownY;
+    const dx = event.clientX - mouseDownX;
+    const dy = event.clientY - mouseDownY;
 
+    if (event.buttons === 1) {
         if (!isDragging && (Math.abs(dx) > DRAG_THRESHOLD || Math.abs(dy) > DRAG_THRESHOLD)) {
             isDragging = true;
         }
@@ -169,16 +169,24 @@ function handleMouseMove(event) {
         }
     }
 
-    if (isRightDragging) {
-        event.preventDefault(); // Prevent default actions during dragging
-        const deltaX = event.clientX - startX;
+    if (event.buttons === 2) {
 
-        fractalApp.rotation = normalizeRotation(fractalApp.rotation + deltaX * ROTATION_SENSITIVITY);
+        if (!isRightDragging && (Math.abs(dx) > DRAG_THRESHOLD || Math.abs(dy) > DRAG_THRESHOLD)) {
+            isRightDragging = true;
+        }
 
-        startX = event.clientX; // Update starting point for smooth rotation
-        canvas.style.cursor = 'grabbing'; // Use a grabbing cursor for rotation
-        fractalApp.draw(); // Redraw with the updated rotation
-        updateInfo();
+        if (isRightDragging) {
+            event.preventDefault(); // Prevent default actions during dragging
+            const deltaX = event.clientX - startX;
+
+            fractalApp.rotation = normalizeRotation(fractalApp.rotation + deltaX * ROTATION_SENSITIVITY);
+            fractalApp.draw(); // Redraw with the updated rotation
+
+            startX = event.clientX; // Update starting point for smooth rotation
+            wasRotated = true;
+            canvas.style.cursor = 'grabbing'; // Use a grabbing cursor for rotation
+            updateInfo();
+        }
     }
 }
 
@@ -209,11 +217,12 @@ function handleMouseUp(event) {
                 clearTimeout(clickTimeout);
                 clickTimeout = null;
 
-                console.log(`%c handleMouseUp: %c Double Left Click: Centering on ${mouseX}x${mouseY} which is fractal coords [${expandComplexToString([fx, fy])}]`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
-
                 const targetZoom = fractalApp.zoom * ZOOM_STEP;
                 if (targetZoom > fractalApp.MAX_ZOOM) {
-                    fractalApp.animatePanAndZoomTo([fx, fy], targetZoom).then(resetAppState);
+                    console.log(`%c handleMouseUp: %c Double Left Click: Centering on ${mouseX}x${mouseY} which is fractal coords [${expandComplexToString([fx, fy])}] and zooming from ${fractalApp.zoom.toFixed(6)} to ${targetZoom}`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
+                    fractalApp.animatePanAndZoomTo([fx, fy], targetZoom, 1000, EASE_TYPE.QUINT).then(resetAppState);
+                } else {
+                    console.log(`%c handleMouseUp: %c Double Left Click: Over max zoom. Skipping,`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
                 }
             } else {
                 // Set a timeout for the single-click action.
@@ -250,7 +259,10 @@ function handleMouseUp(event) {
         if (isRightDragging) {
             isRightDragging = false;
 
-            resetAppState();
+            if (DEBUG_MODE) console.log(`%c handleMouseUp: %c Single Right Click: Doing nothing.`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
+
+            if (wasRotated) resetAppState();
+            wasRotated = false;
             // Reset cursor to default after rotation ends
             canvas.style.cursor = 'crosshair';
             return; // Prevent further processing if it was a drag
@@ -267,10 +279,12 @@ function handleMouseUp(event) {
             clearTimeout(clickTimeout);
             clickTimeout = null;
 
-            console.log("Double Right Click: Zooming out");
             const targetZoom = fractalApp.zoom / ZOOM_STEP;
             if (targetZoom < fractalApp.MIN_ZOOM) {
+                console.log(`%c handleMouseUp: %c Double Right Click: Centering on ${mouseX}x${mouseY} which is fractal coords [${expandComplexToString([fx, fy])}] and zooming from ${fractalApp.zoom.toFixed(6)} to ${targetZoom}`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
                 fractalApp.animatePanAndZoomTo([fx, fy], targetZoom).then(resetAppState);
+            } else {
+                console.log(`%c handleMouseUp: %c Double Right Click: Over min zoom. Skipping,`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, 'color: #fff');
             }
         } else {
             // Set timeout for single click
