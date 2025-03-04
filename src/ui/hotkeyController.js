f88413747ebbd9b65a6a8e1daf52e07459dfd92c
Radim Brnka <71871287+rbrnka@users.noreply.github.com>
2025-03-04 16:43:58 +0100

    hotkeys for MAC, ctrl switchd to Alt

--- src/ui/hotkeyController.js
+++ src/ui/hotkeyController.js
@@ -19,8 +19,16 @@ import {
     travelToPreset,
     updateColorTheme,
 } from "./ui";
-import {DEBUG_MODE, DEFAULT_CONSOLE_GROUP_COLOR, FRACTAL_TYPE, ROTATION_DIRECTION} from "../global/constants";
+import {
+    DEBUG_MODE,
+    DEFAULT_CONSOLE_GROUP_COLOR,
+    DEFAULT_JULIA_THEME_COLOR,
+    DEFAULT_MANDELBROT_THEME_COLOR,
+    FRACTAL_TYPE,
+    ROTATION_DIRECTION
+} from "../global/constants";
 
+//region CONSTANTS > ---------------------------------------------------------------------------------------------------
 /**
  * Keys not allowed to work in animation mode
  * @type {string[]}
@@ -28,28 +36,65 @@ import {DEBUG_MODE, DEFAULT_CONSOLE_GROUP_COLOR, FRACTAL_TYPE, ROTATION_DIRECTIO
 const ANIMATION_KEYS_BLACKLIST = ["ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown", "Space", "KeyQ", "KeyW"]; // TODO consider allowing rotation in Julia mode as it doesnt collide with the demo mode
 
 /**
- * Pan movement duration
+ * Pan movement animation duration
  * @type {number}
  */
 const PAN_SPEED = 50;
 
 /**
+ * Pan step size per keystroke
+ * @type {number}
+ */
+const PAN_STEP = .1;
+
+/**
+ * Smooth (Shift) pan step size per keystroke
+ * @type {number}
+ */
+const PAN_SMOOTH_STEP = .01;
+
+/**
+ * Normal rotation animation step size
+ * @type {number}
+ */
+const ROTATION_ANIMATION_STEP = .1;
+
+/**
+ * Smooth rotation animation step size
+ * @type {number}
+ */
+const ROTATION_ANIMATION_SMOOTH_STEP = .01;
+
+/**
+ * Zoom step per keystroke
+ * @type {number}
+ */
+const ZOOM_ANIMATION_STEP = .1;
+
+/**
+ * Smooth (Shift) zoom step per keystroke
+ * @type {number}
+ */
+const ZOOM_ANIMATION_SMOOTH_STEP = .01;
+
+/**
  * Smooth stepping: step size
  * @type {number}
  */
-const JULIA_HOTKEY_C_STEP = 0.0005;
+const JULIA_HOTKEY_C_STEP = .0005;
 
 /**
  * Super smooth stepping multiplier (SHIFT)
  * @type {number}
  */
-const JULIA_HOTKEY_C_SMOOTH_STEP = 0.1;
+const JULIA_HOTKEY_C_SMOOTH_MULTIPLIER = .1;
 
 /**
  * Smooth stepping: animation duration
  * @type {number}
  */
 const JULIA_HOTKEY_C_SPEED = 10;
+//endregion ------------------------------------------------------------------------------------------------------------
 
 let rotationActive = false;
 let initialized = false;
@@ -63,15 +108,18 @@ let fractalApp;
 async function onKeyDown(event) {
     //event.preventDefault();
 
-    if (DEBUG_MODE) console.log(`%c onKeyDown: %c Pressed ${event.shiftKey ? 'Shift + ' : ''}${event.ctrlKey ? 'CTRL + ' : ''}${event.code}`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, `color: #fff`);
+    if (DEBUG_MODE) console.log(`%c onKeyDown: %c Pressed key/code ${event.shiftKey ? 'Shift + ' : ''}${event.ctrlKey ? 'CTRL + ' : ''}${event.altKey ? 'ALT + ' : ''}${event.code}/${event.key}`, `color: ${DEFAULT_CONSOLE_GROUP_COLOR}`, `color: #fff`);
 
-    const rotationSpeed = event.shiftKey ? 0.01 : 0.1;
+    const rotationSpeed = event.shiftKey ? ROTATION_ANIMATION_SMOOTH_STEP : ROTATION_ANIMATION_STEP;
     let rotationDirection = ROTATION_DIRECTION.CW;
 
     let deltaPanX = 0;
     let deltaPanY = 0;
     let deltaCx = 0;
     let deltaCy = 0;
+
+    // Treat MAC Option button as Alt
+    const altKey = event.altKey || event.code === 'Alt';
 
     if (isAnimationActive() && ANIMATION_KEYS_BLACKLIST.includes(event.code)) return;
 
@@ -107,8 +155,11 @@ async function onKeyDown(event) {
             event.preventDefault();
             event.stopPropagation();
 
-            if (event.altKey) {
-                await fractalApp.animateColorPaletteTransition(fractalApp.DEFAULT_PALETTE, 250, updateColorTheme);
+            if (altKey) {
+                await fractalApp.animateColorPaletteTransition(
+                    isJuliaMode() ? DEFAULT_JULIA_THEME_COLOR : DEFAULT_MANDELBROT_THEME_COLOR,
+                    250,
+                    updateColorTheme);
             } else if (event.shiftKey) {
                 await fractalApp.animateFullColorSpaceCycle(15000);
             } else {
@@ -129,27 +180,32 @@ async function onKeyDown(event) {
             break;
 
         case "ArrowLeft":
-            deltaPanX = event.ctrlKey ? 0 : -(event.shiftKey ? 0.01 : 0.1); // TODO make constants
-            deltaCx = event.ctrlKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? JULIA_HOTKEY_C_SMOOTH_STEP : 1) : 0;
+            deltaPanX = altKey ? 0 : -(event.shiftKey ? PAN_SMOOTH_STEP : PAN_STEP);
+            deltaCx = altKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? JULIA_HOTKEY_C_SMOOTH_MULTIPLIER : 1) : 0;
             break;
 
         case "ArrowRight":
-            deltaPanX = event.ctrlKey ? 0 : (event.shiftKey ? 0.01 : 0.1);
-            deltaCx = event.ctrlKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? -JULIA_HOTKEY_C_SMOOTH_STEP : -1) : 0;
+            deltaPanX = altKey ? 0 : (event.shiftKey ? PAN_SMOOTH_STEP : PAN_STEP);
+            deltaCx = altKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? -JULIA_HOTKEY_C_SMOOTH_MULTIPLIER : -1) : 0;
             break;
 
         case "ArrowDown":
-            deltaPanY = event.ctrlKey ? 0 : -(event.shiftKey ? 0.01 : 0.1);
-            deltaCy = event.ctrlKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? -JULIA_HOTKEY_C_SMOOTH_STEP : -1) : 0;
+            deltaPanY = altKey ? 0 : -(event.shiftKey ? PAN_SMOOTH_STEP : PAN_STEP);
+            deltaCy = altKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? -JULIA_HOTKEY_C_SMOOTH_MULTIPLIER : -1) : 0;
             break;
 
         case "ArrowUp":
-            deltaPanY = event.ctrlKey ? 0 : (event.shiftKey ? 0.01 : 0.1);
-            deltaCy = event.ctrlKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? JULIA_HOTKEY_C_SMOOTH_STEP : 1) : 0;
+            deltaPanY = altKey ? 0 : (event.shiftKey ? PAN_SMOOTH_STEP : PAN_STEP);
+            deltaCy = altKey ? JULIA_HOTKEY_C_STEP * (event.shiftKey ? JULIA_HOTKEY_C_SMOOTH_MULTIPLIER : 1) : 0;
             break;
 
         case "Space":
-            const zoomFactor = event.ctrlKey ? (event.shiftKey ? 1.01 : 1.1) : (event.shiftKey ? 0.99 : 0.9); // TODO make constants
+            event.preventDefault();
+            event.stopPropagation();
+
+            const increment = event.shiftKey ? ZOOM_ANIMATION_SMOOTH_STEP : ZOOM_ANIMATION_STEP;
+            const zoomFactor = altKey ? (1 + increment) : (1 - increment);
+
             let targetZoom = fractalApp.zoom * zoomFactor;
 
             if (targetZoom > fractalApp.MAX_ZOOM && targetZoom < fractalApp.MIN_ZOOM) {
