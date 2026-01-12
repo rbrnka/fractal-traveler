887ff4b9c98aca51e0a31db8c23a3393ab13e356
Radim Brnka <71871287+rbrnka@users.noreply.github.com>
2026-01-12 19:59:02 +0100

    1.9 Refactor: simplify preset button title logic and remove unused commented-out code

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -782,19 +782,11 @@ function initPresetButtonEvents() {
         const btn = document.createElement('button');
         btn.id = 'preset' + (index);
         btn.className = 'preset';
-        btn.title = (preset.title || ('Preset ' + index)) + (index < 10 ? ` (Num ${index})` : '');
+        btn.title = (preset.title || ('Preset ' + index)) + (index < 10 ? ` (Num ${index})` : ` (${index})`); // TODO keep the ID in the title
         btn.textContent = (preset.title || index).toString();
         btn.addEventListener('click', async () => {
             await travelToPreset(presets, index);
         });
-
-        // Support for 10+ Presets
-        // if (index % 10 === 0) {
-        //     btn.classList.add("first-of-ten");
-        // }
-        // if ((index + 1) % 22 === 0) {
-        //     btn.classList.add("last-of-ten");
-        // }
 
         presetBlock.appendChild(btn);
         presetButtons.push(btn);
