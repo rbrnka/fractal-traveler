1d42caf686aca344c7ffb95fa61cd9fa5f2c6fdf
Radim Brnka <71871287+rbrnka@users.noreply.github.com>
2026-01-26 10:07:02 +0100

    Update CSS import paths to align with new directory structure.

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@
  * @license MIT
  */
 
-import '../css/style.css';
-import '../css/debugPanel.css';
+import './css/style.css';
+import './css/debugPanel.css';
 import {JuliaRenderer} from "./renderers/juliaRenderer";
 import MandelbrotRenderer from "./renderers/mandelbrotRenderer";
 import RiemannRenderer from "./renderers/riemannRenderer";
